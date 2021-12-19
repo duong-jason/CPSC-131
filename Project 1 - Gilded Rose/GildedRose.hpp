a54d2f29eaa2df2329e32042391c44e8065615a4
Jason Duong <reddkingdom@csu.fullerton.edu>
2021-12-18 22:22:37 -0800

    Add files via upload

---
+++ Project 1 - Gilded Rose/GildedRose.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <iostream>
+
+#define MAX_STORAGE 10
+
+using namespace std;
+
+class Item {
+public:
+    string name;
+    int sellIn;
+    int quality;
+
+	Item(): name(""), sellIn(0), quality(0) {}
+	Item(string new_name, int new_sellIn, int new_quality) : name(new_name), sellIn(new_sellIn), quality(new_quality) { }
+};
+
+class GildedRose {
+private:
+	Item* inventory = new Item[MAX_STORAGE];
+	size_t stock;
+
+public:
+	GildedRose() : stock(0) {} // initialize data
+	~GildedRose() { delete[] inventory; } // deallocate data
+
+	// adds another item for sale	
+	void add(const Item& object) {
+		if (stock == MAX_STORAGE) {
+			cout << "LIMIT STORAGE = " << MAX_STORAGE << endl;
+			exit(1);
+		}
+		else inventory[stock++] = object;
+	}
+
+	// returns the number of items currently for sale
+	size_t size() const { return stock; }
+
+	// returns the item at a given index position 
+	Item get(const size_t& shelf) const { return inventory[shelf]; }
+
+	// perform identical to the get function
+	Item operator[](const size_t& shelf) {
+		if (shelf > stock) return {};
+		else return inventory[shelf];
+	}
+};
---
+++ Project 2 - Browser History/BrowserHistory.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+string cmd;
+
+struct Website {
+	string url;
+	size_t size;
+
+	Website() : url(""), size(0) {};
+	Website(const string& u, const size_t& s) : url(u), size(s) {};
+
+	friend fstream& operator>>(fstream& read, Website& page) {
+		read >> cmd;
+		if (cmd == "visit") read >> page.url >> page.size;
+		return read;
+	}
+
+} link;
+
+class BrowserHistory {
+private:
+	list<Website> history;
+	list<Website>::iterator curr;
+	fstream read;
+
+public:
+	BrowserHistory() = default;
+	~BrowserHistory() { history.clear(); };
+
+	void readFile(const string& filename) {
+		read.open(filename);	
+		if (read.is_open()) {
+			while (read >> link) {
+				if (cmd == "visit") visitSite(link.url, link.size);
+				else if (cmd == "back") backButton();
+				else if (cmd == "forward") forwardButton();
+			}
+		} else {
+			cout << "Could not open \'" << filename << "\'\n";
+			exit(1);
+		}
+	}
+
+	// current webpage changes to the previous one in the visit sequence (stays the same if no such page)
+	void backButton() {
+		if (curr != history.begin()) --curr;
+	}
+	
+	// current webpage changes to the next one in the visit sequence (states the same if no such page)
+	void forwardButton() {
+		if (curr != prev(history.end())) ++curr;
+	}
+
+	// user visits a website with a given URL and web page size in bytes
+	void visitSite(const string& url, const size_t& filesize) {
+		if (getHistoryLength() > 1) {
+			while (curr != prev(history.end())) history.pop_back();
+		}
+
+		Website link(url, filesize);
+
+		history.push_back(link);
+		curr = prev(history.end());
+	}
+
+	// returns the current url (returns empty string if no such site)
+	string currentUrl() const { 
+		if (getHistoryLength() == 0) return "";
+		return curr->url;
+	}
+
+	// returns the current webpage's page size (returns 0 if no such site)
+	size_t currentPageSize() const { return curr->size; }
+
+	// returns the length of the webpage history
+	size_t getHistoryLength() const { return history.size(); }
+};
